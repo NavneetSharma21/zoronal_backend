64ec5179f506f06f9f14fc85cfdc73c7b37fb969
Navneet sharma <navneetsharma9340@gmail.com>
2026-05-24 18:13:56 +0530

    fixed files issue

--- app.js
+++ app.js
@@ -22,12 +22,7 @@ app.use(
   })
 );
 
-app.use(
-  fileUpload({
-    useTempFiles: true,
-    tempFileDir: "/tmp/",
-  })
-);
+app.use( fileUpload());
 app.use("/upload", express.static("upload"));
 
 (
