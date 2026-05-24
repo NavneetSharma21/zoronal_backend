d46ae26725e3891be219bcb8318d3d073311d262
Navneet sharma <navneetsharma9340@gmail.com>
2026-05-24 17:59:03 +0530

    modified to add cors

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(cors({
-  origin: "https://zoronal-front-end.vercel.app/",
+  origin: "https://zoronal-front-end.vercel.app",
   credentials: true
 }));
 app.use(
