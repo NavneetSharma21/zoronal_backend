24067ec1a5ce7634522f6c0afc7739d0cad684ad
Navneet sharma <navneetsharma9340@gmail.com>
2026-05-24 17:43:58 +0530

    allowed cors acces to frontend

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(cors({
-  origin: "https://your-frontend.vercel.app",
+  origin: "https://zoronal-front-end.vercel.app/",
   credentials: true
 }));
 app.use(
